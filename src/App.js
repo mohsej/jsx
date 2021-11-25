87d6e5f9c98a690a197efe5c1977ea78582cc078
mohsej <mojvj80@gmail.com>
2021-11-25 11:45:16 +0100

    jsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import React from 'react';
+import image1 from './imageInSrc.jpg';
+import myVideo from "./myVideo.mp4";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="flex">
+  <div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
+    <h1 className="title">Hello</h1>
+    <br />
+    <img src={image1} alt="imagesrc"/>
+    <br />
+    <img src="./imageInPublic.png" alt="imagepub"/>
+  </div>
+  <video width={320} height={240} controls>
+    <source src={myVideo} type="video/mp4" />
+  </video>
+</div>
+) ;
 }
 
 export default App;
